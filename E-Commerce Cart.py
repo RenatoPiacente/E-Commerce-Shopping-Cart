fefa6843be2537577c264c71f6caed22ccfba283
Renato <piacente.renato@me.com>
2022-10-09 10:47:20 -0400

    Updated the script by adding a function.

--- E-Commerce Cart.py
+++ E-Commerce Cart.py
@@ -1,10 +1,16 @@
-cart = []
+def shopping_cart():
+    """Gets each product that was added to the cart, by appending the name and price to an empty list.
+    Sums the prices of all products that were added to the cart.
+    Returns the total price.
+    """
+    cart = []
 
-cart.append(('Shirt', 30.50))
-cart.append(('Jeans', 40.99))
-cart.append(('Sneakers', 98.95))
+    cart.append(('Shirt', 31.50))
+    cart.append(('Jeans', 40.99))
+    cart.append(('Sneakers', 97.95))
 
-total = sum([price for product, price in cart])
+    total = sum([price for product, price in cart])
+    return total
 
 if __name__ == '__main__':
-    print(f'Total of your purchase: ${total:.2f}')
+    print(f'Total of your purchase: ${shopping_cart():.2f}')
